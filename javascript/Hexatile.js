77d57168fe83fdaf4d41c1030b449a69505eb1c2
r.tweity@gmail.com <r.tweity@gmail.com@0a73e6cf-a9e8-dec2-bcb0-e5d8b5940376>
2011-11-10 13:28:09 +0000

    Fehlende Semikolon hinzugefügt Zeit wird jetzt angehalten wenn Spiel Verloren

--- javascript/Hexatile.js
+++ javascript/Hexatile.js
@@ -69,7 +69,7 @@ Hexatile.prototype.draw = function(ctx) {
 
 	} else if(this.isOpen == true && this.isMine == false) {
 
-		// In diesen Zeilen wird abhŠngig von der Anzahl der Nachbarminen die Hintergrundfarbe des Hexagon gesetzt
+		// In diesen Zeilen wird abhï¿½ngig von der Anzahl der Nachbarminen die Hintergrundfarbe des Hexagon gesetzt
 		if(this.surroundingMines == 0)
 			ctx.fillStyle = fs0Mines;
 		if(this.surroundingMines == 1)
@@ -119,7 +119,7 @@ Hexatile.prototype.draw = function(ctx) {
 	// Hier wird das Hexatile mit einer Zahl versehen, sollte es aufgedeckt sein, keine 
 	// Mine enthalten und mindestens eine Mine als Nachbarn habn.
 	else if(this.isOpen && (!this.isMine) && this.surroundingMines > 0) {
-		// TODO schšner machen
+		// TODO schï¿½ner machen
 		ctx.fillStyle = fsText;
 		ctx.fillText(this.surroundingMines, cellVector.x + (cellWidth/2), cellVector.y+(cellHeight/2));
 	}
@@ -129,7 +129,7 @@ Hexatile.prototype.draw = function(ctx) {
 
 
 /**
- * Wird ausgefŸhrt, wenn auf die Zelle geklickt wurde
+ * Wird ausgefï¿½hrt, wenn auf die Zelle geklickt wurde
  */
 Hexatile.prototype.clicked = function() {
 	// Wenn die Zelle bereits angeklickt wurde, dann muss nichts gemacht werden
@@ -150,7 +150,7 @@ Hexatile.prototype.clicked = function() {
 
 		alive = false;
 		repaint();
-		
+		time.Stop();
 		alert('Sie haben verloren!');
 		return;
 	} 
@@ -158,7 +158,7 @@ Hexatile.prototype.clicked = function() {
 	// Wenn die Zelle keine Mine ist, dann ...
 	this.surroundingMines = countSurroundingMines(this.line, this.column);
 
-	// Wenn es keine Nachbarminen gibt, mŸssen ale Nachbarzellen geklickt werden
+	// Wenn es keine Nachbarminen gibt, mï¿½ssen ale Nachbarzellen geklickt werden
 	if (this.surroundingMines == 0) {
 		clickSurroundingMines(this.line, this.column);
 	}
@@ -168,7 +168,7 @@ Hexatile.prototype.clicked = function() {
 
 
 /**
- * Gibt einen String zurŸck, welcher die Daten der Zelle textuell wiedergibt
+ * Gibt einen String zurï¿½ck, welcher die Daten der Zelle textuell wiedergibt
  */
 Hexatile.prototype.toString = function() {
 	//TODO generate and return nice string
@@ -182,12 +182,12 @@ Hexatile.prototype.toString = function() {
 Hexatile.prototype.collides = function(vector) {
 	var cellVector = offsetVector.add(lineVector.mult(this.line).add(columnVector.mult(this.column)));
 
-	// Erst wird geprüft, ob der Klick innerhalb des Rechtecks stattfand, welches dieses Hexagon beschreibt
+	// Erst wird geprï¿½ft, ob der Klick innerhalb des Rechtecks stattfand, welches dieses Hexagon beschreibt
 	if(vector.x > cellVector.x && vector.x < (cellVector.x + cellWidth) &&
 			vector.y > cellVector.y && vector.y < (cellVector.y + cellHeight)) {
 
 		/**
-		 * Im folgenden wird ŸberprŸft, ob der Klick in einem der vier au§erhalb des Hexagon liegenden
+		 * Im folgenden wird ï¿½berprï¿½ft, ob der Klick in einem der vier auï¿½erhalb des Hexagon liegenden
 		 * Dreiecke gelandet ist. Wenn ja, dann wurde das Hexagon nicht getroffen.
 		 */
 		// oben links
