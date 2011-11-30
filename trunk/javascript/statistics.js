e3f3143e337441a9f8fb1440a91a9f0dea1a671a
chris-blank@freenet.de <chris-blank@freenet.de@0a73e6cf-a9e8-dec2-bcb0-e5d8b5940376>
2011-11-30 11:52:54 +0000

    Debug Ausgabe entfernt

--- trunk/javascript/statistics.js
+++ trunk/javascript/statistics.js
@@ -211,7 +211,6 @@ function Statistics() {
 			// dann sie verlorenen
 			if(discoveredPercentLost[difficulty]) {
 				games = games + this.getGames(difficulty, this.state.lose);
-				alert(games);
 				totalPercent = totalPercent + (this.getGames(difficulty, this.state.lose) * discoveredPercentLost[difficulty]);
 			}
 			// dann die gewonnenen
