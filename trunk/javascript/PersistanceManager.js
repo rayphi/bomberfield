9b0c82d7fccdbc99a71b1a96108b15d083f30ea2
ichShane@gmail.com <ichShane@gmail.com@0a73e6cf-a9e8-dec2-bcb0-e5d8b5940376>
2011-12-08 09:16:36 +0000

    #25 Ich habe sie statistics so erweitert, dass jedes update direkt an PersistanceManager.updateOverallStatistics() weitergegeben wird. Diese Funktion verwendet den DatabasePersister, wenn dieser eine DB zur verfügung hat, um die globalen Statistiken in der Datenbank zu aktualisieren.

--- trunk/javascript/PersistanceManager.js
+++ trunk/javascript/PersistanceManager.js
@@ -11,7 +11,11 @@ function PersistanceManager() {
  * @param Statistics statistics
  */
 PersistanceManager.saveStatistics = function(statistics) {
-	// Aktuell gibt es nur den CookiePersister, deswegen wird dieser einfach benutzt
+	/*
+	 *  TODO hier muss geklärt werden, welcher Persister unter welcher Bedingung verwendet wird.
+	 */
+	
+	// Daten über den CookiePersister in einem Cookie speichern
 	CookiePersister.saveStatistics(statistics);
 };
 
@@ -23,6 +27,42 @@ PersistanceManager.saveStatistics = function(statistics) {
  * @returns Statistics
  */
 PersistanceManager.loadStatistics = function() {
+	/*
+	 * TODO hier muss geklärt werden, welcher Persister unter welchen Vorraussetzungen
+	 * verwendet werden soll, um das Statistics Objekt auszulesen.
+	 */
+	
 	// Aktuell gibt es nur den CookiePersister, deswegen wird dieser einfach benutzt
 	return CookiePersister.loadStatistics(); 
+};
+
+/**
+ * Diese Funktion benutzt einen DatabasePersister, um die globalen statistiken zu laden,
+ * sollte dies möglich sein. Wenn dies nicht möglich ist, dann gibt diese methode null zurück
+ * 
+ * @returns Statistics, wenn die Statistiken geladen werden können, sonst null
+ */
+PersistanceManager.loadOverallStatistics = function() {
+	// Prüfen, ob der DatabasePersister eine Datenbank zur Verfügung hat
+	if (DatabasePersister.isDatabaseAvailable()) {
+		// Globale Statistiken laden und zurückgeben
+		return DatabasePersister.loadStatistics();
+	}
+	
+	// Wenn keine Datenbank zur Verfügung steht, dann null  zurückgeben
+	return null;
+};
+
+/**
+ * Diese Funktion benutzt Persister, um die übergebenen Daten in Datenbestäde hineinzupflegen,
+ * diese also zu aktualisieren.
+ * 
+ * @param String update die Updates
+ */
+PersistanceManager.updateStatistics = function(update) {
+	// Überprüfen, ob der DatabasePersister eine Datenbank zur Verfügung hat
+	if (DatabasePersister.isDatabaseAvailable()) {
+		// Daten zum updaten übergeben
+		DatabasePersister.updateStatistics(update);
+	}
 };
